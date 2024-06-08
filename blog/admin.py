33dfb0a57f6177dfa4824ceae89137581842df0f
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2024-06-08 02:06:55 +0000

    Add list_display property to show title, slug

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
-class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ("content")
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
