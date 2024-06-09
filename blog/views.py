231ef30d9aa62e43f892700f5014374ecaae8d98
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2024-06-09 18:16:06 +0000

    Create views for favourites and read later

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,11 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 5
+    
+def favourites(request):
+    # Logic to get the favourite posts for the user
+    return render(request, 'favourites.html')
+
+def read_later(request):
+    # Logic to get the posts bookmarked for later read
+    return render(request, 'read_later.html')
