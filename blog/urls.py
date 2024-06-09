6ca3ff2d25466080e93f9b8220899b6ec901d142
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2024-06-09 21:15:45 +0000

    Create the post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from . import views
+from . import views import home, read_later
 from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'), # Root URL pattern
     path('read_later/', read_later, name='read_later'),  # Read Later URL pattern
+    path('add_to_read_later/<int:post_id>/', add_to_read_later, name='add_to_read_later'),  # Add to Read Later URL pattern
 ]
