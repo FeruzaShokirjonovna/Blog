8ee1ab94b570b859449b65bf806aaa9d26026f09
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2024-10-18 23:52:11 +0000

    Add previous button in home page

--- babycare/urls.py
+++ babycare/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
     path("admin/", admin.site.urls),
+    path("", include("blog.urls"), name="blog-urls"),
     path("summernote/", include("django_summernote.urls")),
     path("accounts/", include("allauth.urls")),
+    
 ]
